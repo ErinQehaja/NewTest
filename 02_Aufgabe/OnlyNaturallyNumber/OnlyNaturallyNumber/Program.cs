db35f42a3179bb37eee5fe1430c0c52a652c5b0e
Erin Qehaja <erin.qehaja@student.htldornbirn.at>
2024-09-27 11:28:04 +0200

    Program Done!

---
+++ 02_Aufgabe/OnlyNaturallyNumber/OnlyNaturallyNumber/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class Program {
+    static void Main()
+    {
+        int number;
+        while (true)
+        {
+            Console.Write("Geben Sie eine natürliche Zahl ein!");
+            Console.WriteLine();
+            string input = Console.ReadLine();
+            if(int.TryParse(input, out number) && number > 0) {
+                Console.WriteLine("Sie haben die Zahl {0} eingegeben.", number);
+                break;
+            }
+            else {
+                Console.WriteLine("Das war keine natürliche Zahl!");
+            }
+        }
+    }
+}
