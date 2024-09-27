3be7db13a6736f121693036761bd782997b717b5
Erin Qehaja <erin.qehaja@student.htldornbirn.at>
2024-09-27 11:37:13 +0200

    CleanCode added

--- 02_Aufgabe/OnlyNaturallyNumber/OnlyNaturallyNumber/Program.cs
+++ 02_Aufgabe/OnlyNaturallyNumber/OnlyNaturallyNumber/Program.cs
@@ -3,14 +3,17 @@ class Program {
     static void Main()
     {
         int number;
+
         while (true)
         {
             Console.Write("Geben Sie eine natürliche Zahl ein!");
             Console.WriteLine();
             string input = Console.ReadLine();
-            if(int.TryParse(input, out number) && number > 0) {
+
+            if (int.TryParse(input, out number) && number > 0) {
                 Console.WriteLine("Sie haben die Zahl {0} eingegeben.", number);
             }
+
             else {
                 Console.WriteLine("Das war keine natürliche Zahl!");
             }
