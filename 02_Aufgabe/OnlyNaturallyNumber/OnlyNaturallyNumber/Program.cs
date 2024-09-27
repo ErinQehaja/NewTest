56fa819e8b317f1c6426bf26af30acab9540056b
Erin Qehaja <erin.qehaja@student.htldornbirn.at>
2024-09-27 11:50:32 +0200

    Optimierter Code

--- 02_Aufgabe/OnlyNaturallyNumber/OnlyNaturallyNumber/Program.cs
+++ 02_Aufgabe/OnlyNaturallyNumber/OnlyNaturallyNumber/Program.cs
@@ -3,19 +3,23 @@ class Program {
     static void Main()
     {
         int number;
+        string input = "";
 
-        while (true)
+        while (input != "Beenden")
         {
             Console.Write("Geben Sie eine natürliche Zahl ein!");
             Console.WriteLine();
-            string input = Console.ReadLine();
+            input = Console.ReadLine();
 
             if (int.TryParse(input, out number) && number > 0) {
                 Console.WriteLine("Sie haben die Zahl {0} eingegeben.", number);
             }
 
             else {
-                Console.WriteLine("Das war keine natürliche Zahl!");
+                if (input != "Beenden")
+                {
+                    Console.WriteLine("Das war keine natürliche Zahl!");
+                }
             }
         }
     }
