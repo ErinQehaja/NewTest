9d28d6b682a3bb3883a1d71c1179743bc12d9fef
Erin Qehaja <erin.qehaja@student.htldornbirn.at>
2024-11-08 11:52:04 +0100

    FirstCodeDone

---
+++ ObjektorientierungBegining/Cat.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ObjektorientierungBeginnig
+{
+    class Cat
+    {
+        private string? _color;
+        private DateTime _birthDate;
+
+        public string? Color
+        {
+            get { return _color; }
+            set
+            {
+                if (_color == value) return;
+                _color = value;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                int age = DateTime.Now.Year - _birthDate.Year;
+                if (DateTime.Now < _birthDate.AddYears(age))
+                {
+                    age--;
+                }
+                return age;
+            }
+        }
+
+        public Cat(DateTime birthDate)
+        {
+            _birthDate = birthDate;
+        }
+    }
+}
---
+++ ObjektorientierungBegining/Program.cs
@@ -0,0 +1,10 @@
+﻿using ObjektorientierungBeginnig;
+
+Console.WriteLine("Hello World!");
+
+DateTime birthDate = new DateTime(2020, 5, 15); 
+Cat myCat = new Cat(birthDate); 
+
+myCat.Color = "black";
+Console.WriteLine("The color of my cat is " + myCat.Color);
+Console.WriteLine("The age of my cat is " + myCat.Age + " years");
