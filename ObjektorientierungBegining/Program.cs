ea74ba4ed2324f75836ec1813d5c357c0cd11b42
Erin Qehaja <erin.qehaja@student.htldornbirn.at>
2024-11-08 14:18:47 +0100

    Tierheim DONE!

--- ObjektorientierungBegining/Program.cs
+++ ObjektorientierungBegining/Program.cs
@@ -1,10 +1,25 @@
 ﻿using ObjektorientierungBeginnig;
 
-Console.WriteLine("Hello World!");
+Console.WriteLine("Willkommen im Tierheim!");
 
-DateTime birthDate = new DateTime(2020, 5, 15); 
-Cat myCat = new Cat(birthDate); 
+Tierheim meinTierheim = new Tierheim();
 
-myCat.Color = "black";
-Console.WriteLine("The color of my cat is " + myCat.Color);
-Console.WriteLine("The age of my cat is " + myCat.Age + " years");
+Cat cat1 = new Cat(new DateTime(2020, 5, 15));
+cat1.Color = "schwarz";
+meinTierheim.AddCat(cat1);
+
+Cat cat2 = new Cat(new DateTime(2022, 8, 1));
+cat2.Color = "grau";
+meinTierheim.AddCat(cat2);
+
+meinTierheim.DisplayAllCats();
+
+
+//using ObjektorientierungBeginnig;
+
+//DateTime birthDate = new DateTime(2020, 5, 15); 
+//Cat myCat = new Cat(birthDate); 
+
+//myCat.Color = "black";
+//Console.WriteLine("The color of my cat is " + myCat.Color);
+//Console.WriteLine("The age of my cat is " + myCat.Age + " years");
---
+++ ObjektorientierungBegining/Tierheim.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ObjektorientierungBeginnig
+{
+    class Tierheim
+    {
+        private List<Cat> _cats;
+
+        public Tierheim()
+        {
+            _cats = new List<Cat>();
+        }
+
+        public void AddCat(Cat cat)
+        {
+            _cats.Add(cat);
+            Console.WriteLine("Eine neue Katze wurde aufgenommen.");
+        }
+
+        public void DisplayAllCats()
+        {
+            if (_cats.Count == 0)
+            {
+                Console.WriteLine("Das Tierheim hat keine Katzen.");
+            }
+            else
+            {
+                Console.WriteLine("Im Tierheim befinden sich die folgenden Katzen:");
+                foreach (var cat in _cats)
+                {
+                    Console.WriteLine($"Farbe: {cat.Color}, Alter: {cat.Age} Jahre");
+                }
+            }
+        }
+    }
+}
